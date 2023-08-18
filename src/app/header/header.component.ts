13b2f9159d24985889cdb777174abc2ad51767d2
Jyothi_77 <jyothi@amazeinc.in>
2023-08-18 16:21:10 +0530

    intial

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,12 +19,5 @@ export class HeaderComponent {
     sessionStorage.setItem("isLogin", "no");
     this.router.navigate(['/Login']);
   }
-  changeLanguageID(even: any) {
-    this.loaderService.showSpinner = true;
-    this.LanguageID = even.target.value;
-    sessionStorage.setItem('LanguageID', this.LanguageID);
-    location.reload();
-    this.loaderService.showSpinner = false;
-  }
-
+ 
 }
