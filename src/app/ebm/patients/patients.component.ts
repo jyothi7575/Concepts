799b818f4e03a879d1c27dc19f8822cc3b03adf8
Jyothi_77 <jyothi@amazeinc.in>
2023-08-18 17:40:04 +0530

    patient dashboard

--- src/app/ebm/patients/patients.component.ts
+++ src/app/ebm/patients/patients.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ApiService } from 'src/services/api.service';
+import { LoaderService } from 'src/services/loader.service';
+import { patient } from '../../../assets/jsons/patient';
 @Component({
   selector: 'app-patients',
   templateUrl: './patients.component.html',
@@ -7,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class PatientsComponent {
 
+  labels: any;
+  languageID: any;
+  constructor(public apiService: ApiService, public loaderService: LoaderService, public router: Router) { }
+  ngOnInit() {
+    this.languageID = sessionStorage.getItem('languageID');
+    this.labels = this.languageID == '1' ? patient.english : patient.french;
+  }
+
+  data = [{
+    "name": "hari",
+    "phone": "987543321",
+    "email": "hari@gamil.com",
+    "gender": "female",
+    "address": "moracco"
+
+  }]
 }
---
+++ src/assets/jsons/patient.ts
@@ -0,0 +1,20 @@
+export const patient = {
+    "english": {
+        "name": "Name",
+        "telephone": "Telephone",
+        "email": "EmailID",
+        "gender": "Gender",
+        "address": "Address",
+        "actions": "Actions",
+        "add": "Add",
+    },
+    "french": {
+        "name": "Nom",
+        "telephone": "Téléphone",
+        "email": "EmailID",
+        "gender": "Sexe",
+        "address": "Adresse",
+        "actions": "Actions",
+        "add": "Ajouter",
+    }
+}
