f2aaf9e0e2c5494ee9f20fa392c50a601e866925
Jyothi_77 <jyothi@amazeinc.in>
2023-08-18 16:15:23 +0530

    intial commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,15 +8,24 @@ import { LoaderService } from 'src/services/loader.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  LanguageID: any;
   constructor(public loaderService: LoaderService, public router: Router) { }
-
+  ngOnInit(): void {
+    this.LanguageID = 1;//sessionStorage.getItem('LanguageID');
+  }
   logout() {
     this.loaderService.showSpinner = true;
     sessionStorage.clear();
     this.loaderService.isLogin = 'no';
     sessionStorage.setItem("isLogin", "no");
     this.router.navigate(['/Login']);
+  }
+  changeLanguageID(even: any) {
+    this.loaderService.showSpinner = true;
+    this.LanguageID = even.target.value;
+    sessionStorage.setItem('LanguageID', this.LanguageID);
+    location.reload();
+    this.loaderService.showSpinner = false;
   }
 
 }
