2220973220711923823a274542851e801b611bfe
Jyothi_77 <jyothi@amazeinc.in>
2023-08-21 17:23:34 +0530

    dashboard count

--- src/app/ebm/dashboard/dashboard.component.ts
+++ src/app/ebm/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/services/api.service';
 import { LoaderService } from 'src/services/loader.service';
-
+import { patient } from '../../../assets/jsons/patient';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -10,10 +10,13 @@ import { LoaderService } from 'src/services/loader.service';
 })
 export class DashboardComponent implements OnInit, AfterViewInit {
 
+  languageID: any;
+  labels: any;
   constructor(public apiService: ApiService, public loaderService: LoaderService, public router: Router) { }
-
   ngOnInit(): void {
     this.loaderService.showSpinner = true;
+    this.languageID = this.apiService.languageID;
+    this.labels = this.languageID == '1' ? patient.english : patient.french;
   }
 
   ngAfterViewInit() {
