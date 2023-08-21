4a8fdc147880b6f1f7a39eb293f2b48a47849914
Sanath S <sanath@amazeinc.in>
2023-08-21 18:43:28 +0530

    login page changes

--- src/assets/jsons/login.ts
+++ src/assets/jsons/login.ts
@@ -6,13 +6,15 @@ export const login = {
         "pin": "Pin Code",
         "pleaseWait": "Please wait...",
         "weareFetching": "We are Fetching Data",
+        "forgot": "Forgot Password ?"
     },
     "french": {
-        "login": "SE CONNECTER",
+        "login": "Se connecter",
         "username": "Nom d'utilisateur",
         "password": "Votre mot de passe",
         "pin": "Code PIN",
         "pleaseWait": "Patientez s'il-vous-plait...",
         "weareFetching": "Nous récupérons vos données.",
+        "forgot": "Mot de passe oublié ?"
     }
 }
