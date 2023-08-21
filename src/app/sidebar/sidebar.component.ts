fc293e32a28ca7ff1cc03611ccd8395b823eb08b
Sanath S <sanath@amazeinc.in>
2023-08-21 16:45:07 +0530

    side bar labels made language based

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/services/api.service';
 import { LoaderService } from 'src/services/loader.service';
+import { sidebar } from '../../assets/jsons/sidebar';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  labels: any;
 
   constructor(public loaderService: LoaderService, public apiService: ApiService, public router: Router) { }
+
+  ngOnInit() {
+    this.labels = this.apiService.languageID == '1' ? sidebar.english : sidebar.french;
+  }
 
   navigate(url: any) {
     this.router.navigate([`/${this.apiService.controller}/${url}`]);
   }
-  navigate1(url:any){
+  navigate1(url: any) {
     this.router.navigate([`/${this.apiService.controller1}/${url}`]);
   }
 
