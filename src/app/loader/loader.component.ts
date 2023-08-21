fed84c7f67cf44673924d4714461e7b99ebb3b41
Jyothi_77 <jyothi@amazeinc.in>
2023-08-21 18:04:27 +0530

    loader chnages

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { login } from '../../assets/jsons/login';
+import { ApiService } from 'src/services/api.service';
+import { LoaderService } from 'src/services/loader.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.css']
 })
 export class LoaderComponent {
+  labels: any;
+
+  constructor(public apiService: ApiService, public loaderService: LoaderService, public router: Router) { }
+
+  ngOnInit() {
+    this.labels = this.apiService.languageID == '1' ? login.english : login.french;
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.loaderService.showSpinner = false;
+    }, 500);
+  }
 
 }
