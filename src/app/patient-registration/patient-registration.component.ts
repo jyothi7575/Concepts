46ffa7bb450fa4d980c666afaddfecd6f58c8eb8
Jyothi_77 <jyothi@amazeinc.in>
2023-09-26 11:21:09 +0530

    patient registration label changes

--- src/app/patient-registration/patient-registration.component.ts
+++ src/app/patient-registration/patient-registration.component.ts
@@ -26,44 +26,22 @@ export class PatientRegistrationComponent {
   }
   async clearForm() {
     this.patientForms = new FormGroup({
-      PatientName: new FormControl("", Validators.required),
-      LastName: new FormControl("", Validators.required),
+      firstName: new FormControl("", Validators.required),
+      lastName: new FormControl("", Validators.required),
       dateOfBirth: new FormControl("",),
-      gender: new FormControl("", Validators.required),
-      MobileNumber: new FormControl("", Validators.required),
-      EmailID: new FormControl("", Validators.required),
+      gender: new FormControl(""),
+      mobileNumber: new FormControl("", Validators.required),
+      email: new FormControl("", Validators.required),
       subscriptionNumber: new FormControl("", Validators.required),
       policyNumber: new FormControl("", Validators.required),
-      policyExpiryDate: new FormControl("", Validators.required),
+      policyExpiryDate: new FormControl(""),
       insuranceCompanyName: new FormControl(""),
-      identityNumber:new FormControl(""),
-      Password: new FormControl(""),
-      OTP: new FormControl(""),
-      AdultBit: new FormControl(""),
-      PatientID: new FormControl(""),
-      DownloadType: new FormControl(""),
-      CountryCodeNew: new FormControl(""),
-      CountryID: new FormControl(""),
-      PromoCodeID: new FormControl(""),
-      PromoCode: new FormControl("")
+      identityNumber: new FormControl("", Validators.required)
     });
   }
 
   insertPatient() {
-    /* PatientName = PatientRegistrationEntity.PatientName,
-                    MobileNumber = PatientRegistrationEntity.MobileNumber,
-                    EmailID = PatientRegistrationEntity.EmailID,
-                    Password = PatientRegistrationEntity.Password,
-                    OTP = PatientRegistrationEntity.OTP,
-                    AdultBit = PatientRegistrationEntity.AdultBit,
-                    RelationshipTypeID = PatientRegistrationEntity.RelationshipTypeID,
-                    PatientID = PatientRegistrationEntity.PatientID,
-                    LastName = PatientRegistrationEntity.LastName,
-                    DownloadType = PatientRegistrationEntity.DownloadType,
-                    CountryCodeNew = PatientRegistrationEntity.CountryCodeNew,
-                    CountryID = PatientRegistrationEntity.CountryID,
-                    PromoCodeID = PatientRegistrationEntity.PromoCodeID,
-                    PromoCode = PatientRegistrationEntity.PromoCode */
+
   }
   routerLink() {
     this.router.navigate(['/Patients']);
