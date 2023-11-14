a21cdd0903f5be66dcb6e0ee0165abbe07981af1
Jyothi_77 <jyothi@amazeinc.in>
2023-11-14 14:25:22 +0530

    claims api

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -7,7 +7,7 @@ import axios from 'axios';
 export class ApiService {
 
   hostURL: string;
-
+  hostURL1:string;
   controller: string = sessionStorage.getItem('controller') || "EBM"; 
   controller1: string = sessionStorage.getItem('controller1') || "Claims"; 
 
@@ -15,10 +15,15 @@ export class ApiService {
 
   constructor() {
     this.hostURL = environment.host;
+    this.hostURL1="https://test.epega-sante.com/v1/VoilaDoc/"
   }
 
   public async commonPostCall(endPoint: string, data: any) {
     return await axios.post(this.hostURL  + endPoint, data);
+  }
+
+  public async commonPostCall1(endPoint: string, data: any) {
+    return await axios.post(this.hostURL1  + endPoint, data);
   }
 
   public async commonGetCall(endPoint: string) {
